222ab9b35c36daf9dcd98c81bf33d5509eaa5416
MJ Ghani <ag001c4956@gmail.com>
2016-12-11 19:41:13 +0000

    Added notice for selecting membership plan

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :select_plan, only: :new
   #extend default devise gem behaviour so that
   #users signing up with  the pro account(Plan Id 2)
   #save with a special stripe subsciption function
@@ -13,6 +14,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
           resource.save
         end
       end  
+    end
+  end
+  
+  private
+  def select_plan
+    unless (params[:plan] == '1' || params[:plan] == '2')
+      flash[:notice] = "Please select a membership plan to sign up."
+      redirect_to root_url
     end
   end
 end
