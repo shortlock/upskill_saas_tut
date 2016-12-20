f9a80758d99e42a17a2d89b09d6b80b96be9215b
MJ Ghani <ag001c4956@gmail.com>
2016-12-20 22:24:08 +0000

    Added barebones user show page, added profiles redirect after creating it and checked for user logged in/exists beforeloading view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,28 @@
 class UsersController < ApplicationController
+  before_action :select_user, :is_logged_in? , only: :show
   
   # GET to /users/:id
   def show
     @user = User.find( params[:id])
+    @profile = Profile.where(user_id: params[:id])
+
   end
   
+  def index
+    @user = User.all
+    @profile = Profile.all
+  end
+  private
+  def select_user
+    unless User.exists?(params[:id])
+      flash[:notice] = "User does not exist."
+      redirect_to root_url
+    end
+  def is_logged_in?
+    unless user_signed_in?
+      flash[:notice] = "Please log in to view profiles."
+      redirect_to root_url
+    end
+  end
+  end
 end
