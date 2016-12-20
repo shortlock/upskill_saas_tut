c0dccb4cb7b1f1f076c316460bdc3c7108613329
MJ Ghani <ag001c4956@gmail.com>
2016-12-20 08:49:38 +0000

    Added profiles create action

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,10 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  
+  # Profile form validations
+  validates :first_name, presence: true
+  validates :first_name, presence: true
+  validates :phone_number, presence: true
+  validates :contact_email, presence: true
+  validates :description, presence: true
 end
